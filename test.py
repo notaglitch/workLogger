eadb4f3256b086450ed3c927321122b87217bdce
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-07 10:40:08 +0100

    test.file improved

--- test.py
+++ test.py
@@ -1,22 +1,19 @@
 import pandas as pd
 from datetime import datetime as dt
 
-# Change sheet name to your sheet name
+# Define the sheet name
 sheet_name = "main"
-
-# Today's date, change format to your desired format
-today_date = datetime.today().strftime('%d-%m-%Y')
+# Today's date
+today_date = dt.today().strftime('%d-%m-%Y')
 
 open_file = pd.read_excel("Data.xlsx", sheet_name=sheet_name)
-try:
-    first_empty_row = open_file[open_file.iloc[:,0].isna()].index[0]
-    last_filled_row = first_empty_row - 1
-    last_row_date = open_file.iloc[last_filled_row, 0].strftime('%d-%m-%Y')
-    print(last_row_date)
-except IndexError:
-    last_filled_row = len(open_file) - 1
-    last_row_date = open_file.iloc[last_filled_row, 0].strftime('%d-%m-%Y')
+
+last_row_date = open_file.iloc[-1, 0].strftime('%d-%m-%Y')
+
+def write_to_file(last_row_date, today_date):
     if last_row_date == today_date:
-        print("Today's date is already in the file")
+        print("Up to date")
     else:
-        print(today_date)
+        print("Not up to date")
+
+write_to_file(last_row_date, today_date)
