e0a7fdcee2cd94d9512bb9ea6e4ecc91614a9f88
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-01-28 21:24:22 +0100

    updated version 1

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 from openpyxl import workbook, load_workbook
 import datetime as dt
 
-today_date = dt.today().strftime('%d-%m-%Y')
+today_date = dt.datetime.today().strftime('%d/%m/%Y')
 wb = load_workbook("Data.xlsx")
-ws = wb.active()
+ws = wb.active
 
-for rows in wn
+date_col = ws['A1'].value
+focus_session_col = ws['B1'].value
+last_row = ws.max_row
+
+ws[f"A{last_row}"] = today_date
 
 wb.save("Data.xlsx")
